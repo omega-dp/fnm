5432f65789589e2b9fcac0d5e68a3895d1c6f7e6
muli <muli@datax.datax.com>
2023-07-02 20:59:21 +0300

    this is it

---
+++ ui/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex h-screen bg-gray-50 dark:bg-gray-900" id="app">
+    <Sidebar :isSideMenuOpen="isSideMenuOpen" @close-side-menu="isSideMenuOpen = false" />
+
+    <div class="flex flex-col flex-1 w-full">
+      <Navigation @open-side-menu="isSideMenuOpen = !isSideMenuOpen" />
+      <div class="p-6 dark:text-white">
+        <RouterView />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import "@/assets/main.min.css"
+
+// import { useAuthStore } from './stores/'
+
+import { RouterView } from 'vue-router'
+import Sidebar from './components/Sidebar.vue'
+import Navigation from './components/Navigation.vue'
+
+// const authStore = useAuthStore();
+const isSideMenuOpen = ref(false);
+</script>
---
+++ ui/src/components/Card.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="px-4 py-3 mb-8 bg-white rounded-xl shadow-md dark:bg-gray-800">
+    <slot></slot>
+  </div>
+</template>
+<script setup>
+</script>
