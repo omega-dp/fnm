d075284d990251fb3d473ae1c287248fa18b8221
muli <muli@muli.dev>
2023-07-10 09:28:11 +0300

    new on gitlab

---
+++ fnm/notifications/__init__.py
@@ -0,0 +1,3 @@
+from .core import notify
+
+default_app_config = 'fnm.notifications.apps.NotificationsConfig'
--- fnm/notifications/models.py
+++ fnm/notifications/models.py
@@ -1,45 +1,88 @@
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
+import logging
+
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-from django.conf import settings
-from fnm.common.models import BaseModel
+from .managers import NotificationManager
 
+logger = logging.getLogger(__name__)
 
-class Notification(BaseModel):
-    TRIGGER_CHOICES = [
-        ('leave_created', 'Leave Request Created'),
-        ('ticket_status_change', 'Ticket Status Change'),
-        ('job_card_escalated', 'Job Card Escalated'),
-    ]
-
-    STATUS_CHOICES = [
-        ('created', 'Created'),
-        ('sent', 'Sent'),
-        ('delivered', 'Delivered'),
-        ('read', 'Read'),
-    ]
-
-    trigger = models.CharField(max_length=50, choices=TRIGGER_CHOICES)
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField()
-    priority = models.PositiveIntegerField(blank=True, default=0)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='created')
-
-    # Additional fields for associating notifications with other models
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    related_request = GenericForeignKey('content_type', 'object_id')
-
-    class Meta:
-        db_table = "Notifications"
-        ordering = ['-created_at']
-        indexes = [
-            models.Index(fields=['recipient']),
-            models.Index(fields=['created_at']),
-        ]
-
-    def __str__(self):
-        return f"{self.trigger} Notification for {self.recipient.username}"
+User = get_user_model()
 
 
+class Notification(models.Model):
+    """Notification to a user within Auth"""
+
+    NOTIFICATIONS_MAX_PER_USER_DEFAULTour = 50
+    NOTIFICATIONS_REFRESH_TIME_DEFAULT = 30
+
+    class Level(models.TextChoices):
+        """A notification level."""
+
+        DANGER = 'danger', _('danger')  #:
+        WARNING = 'warning', _('warning')  #:
+        INFO = 'info', _('info')  #:
+        SUCCESS = 'success', _('success')  #:
+
+        @classmethod
+        def from_old_name(cls, name: str) -> object:
+            """Map old name to enum.
+
+            Raises ValueError for invalid names.
+            """
+            name_map = {
+                "CRITICAL": cls.DANGER,
+                "ERROR": cls.DANGER,
+                "WARN": cls.WARNING,
+                "INFO": cls.INFO,
+                "DEBUG": cls.SUCCESS,
+            }
+            try:
+                return name_map[name]
+            except KeyError:
+                raise ValueError(f"Unknown name: {name}") from None
+
+    # LEVEL_CHOICES = (
+    #     ('danger', 'CRITICAL'),
+    #     ('danger', 'ERROR'),
+    #     ('warning', 'WARN'),
+    #     ('info', 'INFO'),
+    #     ('success', 'DEBUG'),
+    # )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    level = models.CharField(choices=Level.choices, max_length=10, default=Level.INFO)
+    title = models.CharField(max_length=254)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
+    viewed = models.BooleanField(default=False, db_index=True)
+
+    objects = NotificationManager()
+
+    def __str__(self) -> str:
+        return f"{self.user}: {self.title}"
+
+    def save(self, *args, **kwargs):
+        # overriden save to ensure cache is invalidated on very call
+        super().save(*args, **kwargs)
+        Notification.objects.invalidate_user_notification_cache(self.user.pk)
+
+    def delete(self, *args, **kwargs):
+        # overriden delete to ensure cache is invaidated on very call
+        super().delete(*args, **kwargs)
+        Notification.objects.invalidate_user_notification_cache(self.user.pk)
+
+    def mark_viewed(self) -> None:
+        """Mark notification as viewed."""
+        logger.info("Marking notification as viewed: %s" % self)
+        self.viewed = True
+        self.save()
+
+    def set_level(self, level_name: str) -> None:
+        """Set notification level according to old level name, e.g. 'CRITICAL'.
+
+        Raises ValueError on invalid level names.
+        """
+        self.level = self.Level.from_old_name(level_name)
+        self.save()
